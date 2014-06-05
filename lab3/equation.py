bc826ca934f7e833e5e9f0c6f0fb9a3086339d0e
largelymfs <largelymfs@largelymfsdeMacBook-Pro.local>
2014-06-05 22:03:43 +0800

    LAB 3 OLS

---
+++ lab3/equation.py
@@ -0,0 +1,58 @@
+import copy
+
+#A Tool using by experiment
+#solve the equation AX=B
+def solve(now_A, now_B):
+	A = copy.deepcopy(now_A)
+	B = copy.deepcopy(now_B)
+
+	m = len(A)
+	if m==0 or m!=len(B):
+		return False, None
+	n = len(A[0])
+	if m!=n :
+		return True, None
+	for i in range(n):
+		#change the A(i,i) to 1
+		delta = float(A[i][i])
+		if delta==0:
+			return True, None
+		for j in range(i, n):
+			A[i][j] /= delta
+		B[i] /= delta
+		#change A(i+..,i) to 0
+		for j in range(i+1, n):
+			delta = A[j][i]
+			for k in range(i, n):
+				A[j][k] += float(-delta) * A[i][k]
+			B[j] += float(-delta) * B[i]
+
+	x = [0.0 for i in range(n)]
+	for i in range(n):
+		now_line = n - i - 1
+		tmp = float(B[now_line])
+		for j in range(now_line+1, n):
+			tmp -= x[j] * A[now_line][j]
+		x[now_line] = tmp
+	return True, x
+
+#check the result of the solution
+def check(A, x, B):
+	m = len(A)
+	n = len(A[0])
+	if len(x)!=n or len(B)!=m:
+		return False
+	for i in range(m):
+		tmp = 0.0
+		for j in range(n):
+			tmp += x[j] * A[i][j]
+		if abs(tmp-float(B[i]))>0.001:
+			return False
+	return True
+
+##check program
+if __name__=="__main__":
+	A=[[2,3,5.0],[-4.0,5,6],[2,4,6]]
+	B=[4, 5,87]
+	print solve(A, B)
+	print check(A, solve(A, B), B)
---
+++ lab3/exp3.py
@@ -0,0 +1,58 @@
+#-*- coding:utf-8 -*-
+#lab 3
+from equation import solve
+from matplotlib import pyplot as plt
+
+def OLS(n, x, y):
+	A = []
+	m = len(x)
+	if m!=len(y):
+		return None
+	for i in range(n + 1):
+		A.append([])
+		for j in range(n + 1):
+			if i <= j:
+				tmp = 0.0
+				for p in range(m):
+					#for q in range(m):
+					tmp += (float(x[p]) ** i) * (float(x[p]) ** j)
+				A[i].append(tmp)
+			else:
+				A[i].append(A[j][i])
+	B = []
+	for i in range(n+1):
+		tmp = 0.0
+		for p in range(m):
+			#for q in range(m):
+			tmp += (float(x[p]) ** i) * y[p]
+		B.append(tmp)
+	flag, answer = solve(A, B)
+	return answer
+
+if __name__=="__main__":
+	x = [20, 25, 30, 35, 40, 45, 50, 55, 60]
+	y = [805, 985, 1170, 1365, 1570, 1790, 2030, 2300, 2610]
+	a = OLS(3, x, y)
+	f = []
+	for i in range(len(x)):
+		tmp = 1.0
+		tmp_f = 0.0
+		for j in range(len(a)):
+			tmp_f += tmp * a[j]
+			tmp *= x[i]
+		f.append(tmp_f)
+	print "Answer : ","\t",
+	for i in range(len(a)):
+		print i,"\t",
+	print
+	print "\t",
+	for i in range(len(a)):
+		print a[i],"\t",
+	print
+	print "Estimation\t Actual Value"
+	for i in range(len(y)):
+		print f[i],"\t",y[i]
+	plt.plot(x, y,'r',label="x-y")
+	plt.plot(x, f, 'g',label="x-f")
+	plt.legend()
+	plt.show()
